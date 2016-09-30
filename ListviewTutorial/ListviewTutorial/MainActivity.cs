93f5221961908e0bcf75bd03067d981dd0342b7b
kweonpaul <kweonpaul@gmail.com>
2016-09-30 16:12:24 +0900

    ListViewTutorial

---
+++ ListviewTutorial/ListviewTutorial/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+
+namespace ListviewTutorial
+{
+    [Activity(Label = "ListviewTutorial", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        private List<string> mItems;
+        private ListView mListView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            
+            SetContentView(Resource.Layout.Main);
+            mListView = FindViewById<ListView>(Resource.Id.MyListView);
+
+            mItems = new List<string>();
+            mItems.Add("Bob");
+            mItems.Add("Paul");
+            mItems.Add("John");
+            
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItems);
+
+            mListView.Adapter = adapter;
+
+        }
+    }
+}
+
---
+++ ListviewTutorial/ListviewTutorial/obj/Debug/android/src/md5bc59a5e6c2613f18ca021240e27c56e2/MainActivity.java
@@ -0,0 +1,47 @@
+package md5bc59a5e6c2613f18ca021240e27c56e2;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("ListviewTutorial.MainActivity, ListviewTutorial, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("ListviewTutorial.MainActivity, ListviewTutorial, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}

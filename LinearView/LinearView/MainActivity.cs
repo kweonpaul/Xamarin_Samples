4cbbf45f78eef9cb035680226d3bfa38eaf9e8fc
kweonpaul <kweonpaul@gmail.com>
2016-11-06 00:23:18 +0900

    LinearView

---
+++ LinearView/LinearView/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace LinearView
+{
+    [Activity(Label = "LinearView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
